99fca55c436bb0ab1c8750ef2cb6f4064d8da118
ajikyasharma <ajikyasharma24@gamil.com>
2024-09-19 15:28:27 +0530

    initial commit

---
+++ src/Components/Form.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import form_bg from '../assets/form_bg.png'
+
+function Form() {
+  return (
+     <div className="bg-white rounded-lg shadow-lg max-w-lg ">
+            {/* <h2 className="text-2xl font-bold text-blue-700 mb-6">Start your Free Trial</h2> */}
+            <div
+        className="w-full h-48 bg-no-repeat rounded-t-lg  bg-cover bg-center flex flex-col items-center justify-center text-white"
+        style={{ backgroundImage: `url(${form_bg})`,
+        clipPath: 'ellipse(120% 100% at 50% 0)' }}
+      >
+        <h1 className="text-3xl font-bold text-center font-PlayFair">Start Your Free Trial</h1>
+        <p className="text-md mt-2 text-center">Start Your Free Trial Today – No Commitment Required.</p>
+        <div className="border-t-2 border-white w-12 mt-4"></div>
+      </div>
+
+      <div className='p-8 mt-8'>
+            <form>
+              <div className="mb-8 md:mb-12">
+                <input
+                  type="text"
+                  id="username"
+                  placeholder="Username"
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-200 focus:outline-none"
+                />
+              </div>
+              <div className="mb-8 md:mb-12">
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Email"
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-200 focus:outline-none"
+                />
+              </div>
+              <div className="mb-8 md:mb-12">
+                <input
+                  type="tel"
+                  id="phone"
+                  placeholder='Phone number'
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-200 focus:outline-none"
+                />
+              </div>
+
+              <div className="mb-8 md:mb-12">
+                <input
+                  type="tel"
+                  id="company name"
+                  placeholder='Company Name'
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-200 focus:outline-none"
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-[#0181DD] text-white font-bold py-2 px-4 rounded-md "
+              >
+                Register
+              </button>
+            </form>
+            </div>
+          </div>
+  )
+}
+
+export default Form
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        Poppins:[ "Poppins", "sans-serif"],
+        LondrinaOutline:["Londrina Outline", "sans-serif"],
+        workSans:[ "Work Sans", "sans-serif"],
+        inter:["Inter", "sans-serif"],
+        PlayFair:["Playfair Display", "serif"]
+      },
+    },
+  },
+  plugins: [],
+}
+
